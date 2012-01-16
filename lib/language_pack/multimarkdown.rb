c327b07b3ba439f87357a3a7d588bdc1eb43c334
Nathan Colgate Clark <nathancolgate@gmail.com>
2012-01-16 13:51:21 -0800

    Cleaning up the cruft from many wrong tests

--- lib/language_pack/multimarkdown.rb
+++ lib/language_pack/multimarkdown.rb
@@ -23,43 +23,18 @@ class LanguagePack::Multimarkdown < LanguagePack::Rails3
   def compile
     super
 
+    topic "Installing multimarkdown"
     FileUtils.mkdir_p(slug_vendor_multimarkdown)
     Dir.chdir(slug_vendor_multimarkdown) do
       run("curl #{VULCAN_BINARY} -s -o - | tar zxf -")
     end
-    error 'Bad News Bears' unless $?.success?
+    error 'Multimarkdown could not be installed' unless $?.success?
 
     bin_dir = "bin"
     FileUtils.mkdir_p bin_dir
     run("cp #{slug_vendor_multimarkdown}/* #{bin_dir}")
     Dir["bin/*"].each {|path| run("chmod +x #{path}") }
 
-
-    # ENV["CFLAGS"] = '-include GLibFacade.h'
-    # # run("curl #{URL} -L -s -o - | tar zxf -")
-    # # Downloads from web and unzips the file
-    # # -L = Follows redirects
-    # # -s = Silent
-    # # -o - | tar zxf - = Take the downloaded data, and unzip it
-    # topic "Git clone peg-multimarkdown"
-    # Dir.chdir(build_path) do
-    #   pipe("git clone #{GIT_REPO}")
-    # end
-    # error 'Uh oh 1' unless $?.success?
-    # 
-    # topic "Make peg-multimarkdown"
-    # Dir.chdir("#{build_path}/peg-multimarkdown") do
-    #   # run("curl #{URL} -L -s -o - | tar zxf -")
-    #   pipe("make")
-    # end
-    # error 'Uh oh 2' unless $?.success?
-    # 
-    # topic "copy peg-multimarkdown"
-    # Dir.chdir("#{build_path}/peg-multimarkdown") do
-    #   # run("curl #{URL} -L -s -o - | tar zxf -")
-    #   pipe("cp multimarkdown /usr/local/bin/multimarkdown")
-    # end
-    # error 'Uh oh 3' unless $?.success?
   end
 
 
