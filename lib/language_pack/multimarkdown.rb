4b1a74886095b8917b6ccd61ed30d297a9e7f03c
Nathan Colgate Clark <nathancolgate@gmail.com>
2012-01-16 14:19:21 -0800

    File cleanup

--- lib/language_pack/multimarkdown.rb
+++ lib/language_pack/multimarkdown.rb
@@ -1,18 +1,15 @@
-## require 'formula'
 require "language_pack"
 
 # Multimarkdown Language Pack
-## class Multimarkdown < Formula
 class LanguagePack::Multimarkdown < LanguagePack::Rails3
   
-  VULCAN_BINARY      = "http://heroku-buildpacks.s3.amazonaws.com/multimarkdown-3.5.tgz"
+  VULCAN_MULTIMARKDOWN_BINARY      = "http://heroku-buildpacks.s3.amazonaws.com/multimarkdown-3.5.tgz"
 
   # name of the Language Pack
   # @return [String] the result
   def name
     "Multimarkdown"
   end
-
   
   # the relative path to the vendored ruby directory
   # @return [String] resulting path
@@ -26,7 +23,7 @@ class LanguagePack::Multimarkdown < LanguagePack::Rails3
     topic "Installing multimarkdown"
     FileUtils.mkdir_p(slug_vendor_multimarkdown)
     Dir.chdir(slug_vendor_multimarkdown) do
-      run("curl #{VULCAN_BINARY} -s -o - | tar zxf -")
+      run("curl #{VULCAN_MULTIMARKDOWN_BINARY} -s -o - | tar zxf -")
     end
     error 'Multimarkdown could not be installed' unless $?.success?
 
@@ -34,8 +31,6 @@ class LanguagePack::Multimarkdown < LanguagePack::Rails3
     FileUtils.mkdir_p bin_dir
     run("cp #{slug_vendor_multimarkdown}/* #{bin_dir}")
     Dir["bin/*"].each {|path| run("chmod +x #{path}") }
-
   end
-
 
 end
