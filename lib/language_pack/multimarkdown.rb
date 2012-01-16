394cd392ffff1cd014e681652dfed1bd2881a8c5
Nathan Colgate Clark <nathancolgate@gmail.com>
2012-01-16 13:40:06 -0800

    Now using vulcan

--- lib/language_pack/multimarkdown.rb
+++ lib/language_pack/multimarkdown.rb
@@ -5,12 +5,7 @@ require "language_pack"
 ## class Multimarkdown < Formula
 class LanguagePack::Multimarkdown < LanguagePack::Rails3
   
-  ## homepage 'http://fletcherpenney.net/multimarkdown/'
-  ## url 'https://github.com/fletcher/peg-multimarkdown/tarball/3.2'
-  ## md5 '438a7c09af13adf91e318ba2f49681b6'
-  ## head 'https://github.com/fletcher/peg-multimarkdown.git', :branch => 'development'
-  # URL           = "https://github.com/fletcher/peg-multimarkdown/tarball/3.2"
-  GIT_REPO      = "git://github.com/fletcher/peg-multimarkdown.git"
+  VULCAN_BINARY      = "http://heroku-buildpacks.s3.amazonaws.com/multimarkdown-3.5.tgz"
 
   # name of the Language Pack
   # @return [String] the result
@@ -18,42 +13,53 @@ class LanguagePack::Multimarkdown < LanguagePack::Rails3
     "Multimarkdown"
   end
 
-  ## def install
-  ##   ENV.append 'CFLAGS', '-include GLibFacade.h'
-  ##   system "make"
-  ##   bin.install 'multimarkdown'
-  ##   bin.install Dir['Support/bin/*']
-  ##   bin.install Dir['scripts/*']
-  ## end
+  
+  # the relative path to the vendored ruby directory
+  # @return [String] resulting path
+  def slug_vendor_multimarkdown
+    "vendor/multimarkdown"
+  end
 
   def compile
     super
-    ENV["CFLAGS"] = '-include GLibFacade.h'
-    
-    # run("curl #{URL} -L -s -o - | tar zxf -")
-    # Downloads from web and unzips the file
-    # -L = Follows redirects
-    # -s = Silent
-    # -o - | tar zxf - = Take the downloaded data, and unzip it
-    topic "Git clone peg-multimarkdown"
-    Dir.chdir(build_path) do
-      pipe("git clone #{GIT_REPO}")
+
+    FileUtils.mkdir_p(slug_vendor_multimarkdown)
+    Dir.chdir(slug_vendor_multimarkdown) do
+      run("curl #{VULCAN_BINARY} -s -o - | tar zxf -")
     end
-    error 'Uh oh 1' unless $?.success?
-    
-    topic "Make peg-multimarkdown"
-    Dir.chdir("#{build_path}/peg-multimarkdown") do
-      # run("curl #{URL} -L -s -o - | tar zxf -")
-      pipe("make")
-    end
-    error 'Uh oh 2' unless $?.success?
-    
-    topic "copy peg-multimarkdown"
-    Dir.chdir("#{build_path}/peg-multimarkdown") do
-      # run("curl #{URL} -L -s -o - | tar zxf -")
-      pipe("cp multimarkdown /usr/local/bin/multimarkdown")
-    end
-    error 'Uh oh 3' unless $?.success?
+    error 'Bad News Bears' unless $?.success?
+
+    bin_dir = "bin"
+    FileUtils.mkdir_p bin_dir
+    run("cp #{slug_vendor_multimarkdown}/* #{bin_dir}")
+    Dir["bin/*"].each {|path| run("chmod +x #{path}") }
+
+
+    # ENV["CFLAGS"] = '-include GLibFacade.h'
+    # # run("curl #{URL} -L -s -o - | tar zxf -")
+    # # Downloads from web and unzips the file
+    # # -L = Follows redirects
+    # # -s = Silent
+    # # -o - | tar zxf - = Take the downloaded data, and unzip it
+    # topic "Git clone peg-multimarkdown"
+    # Dir.chdir(build_path) do
+    #   pipe("git clone #{GIT_REPO}")
+    # end
+    # error 'Uh oh 1' unless $?.success?
+    # 
+    # topic "Make peg-multimarkdown"
+    # Dir.chdir("#{build_path}/peg-multimarkdown") do
+    #   # run("curl #{URL} -L -s -o - | tar zxf -")
+    #   pipe("make")
+    # end
+    # error 'Uh oh 2' unless $?.success?
+    # 
+    # topic "copy peg-multimarkdown"
+    # Dir.chdir("#{build_path}/peg-multimarkdown") do
+    #   # run("curl #{URL} -L -s -o - | tar zxf -")
+    #   pipe("cp multimarkdown /usr/local/bin/multimarkdown")
+    # end
+    # error 'Uh oh 3' unless $?.success?
   end
 
 
